f871c4bba406511d283895a8a06ba787f89ae8e2
austin2118ace <austin2118ace@gmail.com>
2024-06-07 19:22:20 -0400

    Add some default size policies, minimum heights, and rename a few components

--- dewan_manual_curation/gui.py
+++ dewan_manual_curation/gui.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QDialog, QPushButton, QVBoxLayout, QHBoxLayout, QGroupBox, QFrame
+from PySide6.QtWidgets import (QDialog, QPushButton, QVBoxLayout,
+                               QHBoxLayout, QGroupBox, QFrame, QScrollArea, QSizePolicy)
 from PySide6.QtGui import QFont
 from PySide6.QtCore import Qt
 
@@ -7,10 +8,17 @@ class ManualCurationUI(QDialog):
         super().__init__()
         self.default_font = QFont("Arial", 12)
 
+        #  Cell List Components
+        self.cell_scroll_list = None
+        self.select_all_button = None
+        self.select_none_button = None
+
         #  Layouts
         self.main_layout = None
-        self.top_half_layout = None
+        self.top_half_container = None
         self.bottom_half_layout = None
+        self.cell_list_layout = None
+        self.cell_list_control_layout = None
 
         #  Group Boxes
         self.cell_list_box = None
@@ -23,8 +31,8 @@ class ManualCurationUI(QDialog):
 
     def init_window_params(self):
         self.setWindowTitle('Dewan Manual Curation')
-        self.setMinimumSize(400, 400)
-        self.resize(400, 400)
+        self.setMinimumSize(500, 600)
+        # self.resize(400, 400)
         self.setFont(self.default_font)
         self.setWindowFlag(Qt.WindowMinimizeButtonHint, True)
         self.setWindowFlag(Qt.WindowMaximizeButtonHint, True)
@@ -34,14 +42,40 @@ class ManualCurationUI(QDialog):
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
 
-        self.top_half_layout = QHBoxLayout()
-        self.cell_list_box = QGroupBox("Cells")
+        self.top_half_container = QHBoxLayout()  # Holds the cell list and max projection
+
+        self.cell_list_box = QGroupBox("Cells")  # Box for cell scroll area and buttons
+        self.cell_list_box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        self.cell_list_box.setMaximumWidth(250)
+
+        self.cell_list_layout = QVBoxLayout()
+        self.cell_list_box.setLayout(self.cell_list_layout)
+
+        self.cell_scroll_list = QScrollArea()
+        self.cell_list_layout.addWidget(self.cell_scroll_list)  # Add the scroll area to the layout
+
+        self.cell_list_control_layout = QHBoxLayout()
+        self.select_all_button = QPushButton(u"Select All")
+        self.select_all_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.select_none_button = QPushButton(u"Select None")
+        self.select_none_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        self.cell_list_control_layout.addWidget(self.select_all_button)
+        self.cell_list_control_layout.addWidget(self.select_none_button)
+
+        self.cell_list_layout.addLayout(self.cell_list_control_layout)
+
         self.max_projection_box = QGroupBox("Max Projection")
-        self.top_half_layout.addWidget(self.cell_list_box)
-        self.top_half_layout.addWidget(self.max_projection_box)
-        self.main_layout.addLayout(self.top_half_layout)
+        self.max_projection_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.max_projection_box.setMinimumHeight(300)
+
+        self.top_half_container.addWidget(self.cell_list_box)
+        self.top_half_container.addWidget(self.max_projection_box)
+        self.main_layout.addLayout(self.top_half_container)
 
         self.cell_trace_box = QGroupBox("Traces")
+        self.cell_trace_box.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.cell_trace_box.setMinimumHeight(200)
         self.bottom_half_layout = QHBoxLayout()
         self.cell_trace_box.setLayout(self.bottom_half_layout)
 
