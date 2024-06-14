776ae8d57dfbc16ceb747de6fc5381699c8124e6
austin2118ace <austin2118ace@gmail.com>
2024-06-14 18:26:22 -0400

    Update main function call w/ test data

--- dewan_manual_curation/dewan_manual_curation.py
+++ dewan_manual_curation/dewan_manual_curation.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 
 import gui
 from project_folder import ProjectFolder
-
+import pandas as pd
 
 def launch_gui():
 
@@ -11,8 +11,10 @@ def launch_gui():
     qdarktheme.setup_theme('dark')
 
     project_folder = ProjectFolder(project_dir='C:/Projects/Test_Data/VGLUT-20')
+    cell_props = pd.read_csv(project_folder.cell_props_path, header=0, engine='pyarrow')
+    cell_names = cell_props['Name']
 
-    window = gui.ManualCurationUI(project_folder)
+    window = gui.ManualCurationUI(project_folder, cell_names)
     window.show()
 
     result = app.exec()
