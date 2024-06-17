241f9dae7cd1b4df542d236089ad932fa4850f51
austin2118ace <austin2118ace@gmail.com>
2024-06-16 20:55:48 -0400

    Restructure project folder to have more sensible function calls Check for the inscopix data folder at setup time set root folder to "." by default

--- dewan_manual_curation/project_folder.py
+++ dewan_manual_curation/project_folder.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class ProjectFolder:
-    def __init__(self, root_dir="", project_dir=None):
+    def __init__(self, root_dir=".", project_dir=None):
         self.root_dir = root_dir
         self.project_dir = project_dir
 
@@ -16,6 +16,7 @@ class ProjectFolder:
         self.inscopix_path = None
 
         self.setup_folder()
+        self.get_project_files()
 
     def setup_folder(self):
 
@@ -23,8 +24,8 @@ class ProjectFolder:
         root_directory = Path(self.root_dir)
 
         if not root_directory.exists():
-            print(f"Root path {str(root_directory)} does not exist! Setting root path to default!")
-            self.root_dir = ""
+            print(f"User-supplied root path {str(root_directory)} does not exist! Setting root path to default!")
+            self.root_dir = "."
         else:
             self.root_dir = str(root_directory)
 
@@ -33,40 +34,27 @@ class ProjectFolder:
             temp_folder = Path(self.project_dir)
 
             if not temp_folder.exists():
-                raise FileNotFoundError(f'Project folder {str(temp_folder)} does not exist')
+                raise FileNotFoundError(f'User-supplied project folder {str(temp_folder)} does not exist')
             else:
                 self.project_folder = temp_folder
         else:
             #  Get Project Folder from Selector  #
-            project_folder = self.select_project_folder()[0]
-            self.project_folder = Path(project_folder)
+            returned_folder = self.select_project_folder()
+            if len(returned_folder) > 0:
+                project_folder = returned_folder[0]
+                self.project_folder = Path(project_folder)
+            else:
+                raise FileNotFoundError(f'No project folder selected!')
 
-        self.get_project_files()
+        inscopix_path = self.project_folder.joinpath(*['InscopixProcessing', 'DataAnalysis'])
 
-    def select_project_folder(self) -> list[str]:
-        file_names = []
-
-        file_dialog = QFileDialog()
-        file_dialog.setWindowTitle("Select Project Directory:")
-        file_dialog.setFileMode(QFileDialog.FileMode.Directory)
-        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
-        file_dialog.setDirectory(self.root_dir)
-
-        if file_dialog.exec():
-            file_names = file_dialog.selectedFiles()
-
-        return file_names
+        if not inscopix_path.exists():
+            raise FileNotFoundError(f'Inscopix data folder {str(inscopix_path)} does not exist!\n'
+                                    f' Has data processing been run?')
+        else:
+            self.inscopix_path = inscopix_path
 
     def get_project_files(self):
-        self.max_projection_path = None
-        self.cell_trace_data_path = None
-        self.cell_props_path = None
-
-        self.inscopix_path = self.project_folder.joinpath(*['InscopixProcessing', 'DataAnalysis'])
-
-        if not self.inscopix_path.exists():
-            raise FileNotFoundError(f'Data folder {self.inscopix_path} does not exist!')
-
         max_projection_path = list(self.inscopix_path.glob('*HD*MAX_PROJ*.tiff'))
         cell_trace_data_path = list(self.inscopix_path.glob('*TRACES*.csv'))
         cell_props_path = list(self.inscopix_path.glob('*props*.csv'))
@@ -84,6 +72,20 @@ class ProjectFolder:
             self.max_projection_path = max_projection_path[0]
             self.cell_trace_data_path = cell_trace_data_path[0]
             self.cell_props_path = cell_props_path[0]
+            self.cell_contours_path = cell_contours_path[0]
+
+    def select_project_folder(self) -> list[str]:
+        file_names = []
+
+        file_dialog = QFileDialog()
+        file_dialog.setWindowTitle("Select Project Directory:")
+        file_dialog.setFileMode(QFileDialog.FileMode.Directory)
+        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
+        file_dialog.setDirectory(self.root_dir)
+
+        if file_dialog.exec():
+            file_names = file_dialog.selectedFiles()
+        return file_names
 
     #  Dunder Methods  #
     def __str__(self):
