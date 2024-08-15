21fe059358daaac2d57b1c9412bbb89ecd48a08c
DewanLab <dewanlab@psy.fsu.edu>
2024-08-15 11:26:05 -0400

    change local import to package import

---
+++ build/lib/dewan_manual_curation/_components/cell_trace.py
@@ -0,0 +1,100 @@
+""" CellTrace class for displaying calcium transient data """
+
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+from matplotlib.pyplot import Figure
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+
+from PySide6.QtWidgets import QSizePolicy
+from sklearn.preprocessing import MinMaxScaler
+
+
+# Let's set the default font to be Arial Bold 14pt
+mpl.rcParams['font.family'] = 'arial'
+mpl.rcParams['font.weight'] = 'bold'
+mpl.rcParams['font.size'] = 14
+
+
+class CellTrace(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=30, height=1.1, dpi=100, figure_min_max=(0, 1), reference_line=False,
+                 reference_line_color='r'):
+        self.parent = parent
+        self.dpi = dpi
+
+        self.figure_min_max = figure_min_max
+        self.reference_line = reference_line
+        self.reference_line_color = reference_line_color
+        self.reference_line_color = reference_line_color
+        self.trace_name = 'No_Cell_Present'
+
+        self.figure = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.figure.add_subplot(111)
+        super().__init__(self.figure)
+
+    def __str__(self):
+        return f'Manual Curation Trace {{Cell: {self.trace_name}}}'
+
+    def plot_trace(self, trace_data: pd.Series, cell_name):
+
+        self.trace_name = cell_name
+
+        ymin_label = round(trace_data.min(), 4)  # We want the original max/min to display alongside the z-scored data
+        ymax_label = round(trace_data.max(), 4)
+
+        data_2_plot = self._scale_data(trace_data, self.figure_min_max)
+        x_values = np.arange(len(data_2_plot))  # Quicker than list(range(x))
+        self.axes.plot(x_values, data_2_plot, color='k', linewidth=0.5)
+
+        # ==CHANGE APPEARANCE== #
+
+        largest_x = x_values[-1]
+
+        xaxis_offset = largest_x * 0.01
+
+        x_minlim = -xaxis_offset
+        x_maxlim = largest_x + xaxis_offset
+        self.axes.set_xlim([-xaxis_offset, (x_values[-1] + xaxis_offset)])
+        self.axes.set_ylim([0, 1])  # y-values will always be [0, 1]
+        y_line_val = np.mean(data_2_plot)
+        if self.reference_line:
+            self.axes.hlines(y=y_line_val, xmin=x_minlim, xmax=x_maxlim,
+                             linestyles=(0, (5, 10)), colors=self.reference_line_color)
+
+        self.axes.tick_params(axis='both', which='both', left=False, bottom=False)
+        self.axes.set_xticks([], labels=[])
+        self.axes.set_yticks([0, 1], labels=[ymin_label, ymax_label])
+
+        self.axes.get_yaxis().set_label_coords(-0.1, 0.5)  # Align all the things
+        self.axes.yaxis.tick_right()
+
+        self.axes.set_ylabel(f'Cell: {self.trace_name}', rotation=0, va='center', ha='center')
+
+        self._set_trace_sizing()  # Reset sizing after plotting
+
+    def _set_trace_sizing(self):
+        width, height = self.get_width_height()
+        self.setMinimumSize(width/3, height)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding))
+
+    @staticmethod
+    def _scale_data(trace_data: pd.Series, feature_range: tuple):
+
+        _min, _max = feature_range
+
+        scaler = MinMaxScaler(feature_range=(_min, _max))
+        scaled_data = scaler.fit_transform(trace_data.reshape(-1, 1)).ravel()
+
+        return scaled_data
+
+    @staticmethod
+    def generate_cell_traces(cell_trace_data, cell_names):
+        cell_traces = []
+        for cell in cell_names:
+            data = cell_trace_data[cell].values
+            _cell_trace = CellTrace(reference_line=True)
+            _cell_trace.plot_trace(data, cell)
+            cell_traces.append(_cell_trace)
+
+        return cell_traces
---
+++ build/lib/dewan_manual_curation/dewan_manual_curation.py
@@ -0,0 +1,76 @@
+"""
+Name: DewanLab Manual Curation V2
+Author: Austin Pauley (Dewan Lab, FSU)
+Date: 06/2024
+Desc: User interface to enable simultaneous 1-P calcium fluorescence traces (dF/F) along with the
+spatial positioning of different cells. This interface is intended to be launched both from,
+or independent of, the DewanLab InscopixAnalysis.ipynb processing pipeline notebook.
+Version: 2.1
+"""
+
+import qdarktheme
+import pandas as pd
+from PySide6.QtWidgets import QApplication
+# Our Libraries
+from .gui import ManualCurationUI
+from dewan_calcium.helpers.project_folder import ProjectFolder
+from ._components.cell_trace import CellTrace
+from dewan_calcium.helpers import parse_json
+
+
+def launch_gui(project_folder_override=None, cell_trace_data_override=None, cell_names_override=None,
+               cell_contours_override=None):
+
+    if project_folder_override is None:
+        project_folder = ProjectFolder(select_dir=True)
+    else:
+        project_folder = project_folder_override
+
+    cell_trace_data, cell_names, cell_contours = get_data(project_folder, cell_trace_data_override,
+                                                          cell_names_override, cell_contours_override)
+
+    cell_traces = CellTrace.generate_cell_traces(cell_trace_data, cell_names)
+
+    app = QApplication.instance()
+
+    if not app:
+        app = QApplication([])
+
+    qdarktheme.setup_theme('dark')
+
+    window = ManualCurationUI(cell_names, cell_traces, cell_contours,
+                              project_folder.inscopix_dir.max_projection_path)
+    window.show()
+
+    return_val = app.exec()
+
+    if return_val == 0:  # 0: Success! | 1: Failure!
+        return window.curated_cells
+    else:
+        return None
+
+
+def get_data(project_folder, cell_trace_data_override, cell_names_override, cell_contours_override):
+
+    if cell_trace_data_override is None:
+        cell_trace_data = []
+        pass  # Load cell trace data from pickle
+    else:
+        cell_trace_data = cell_trace_data_override
+
+    if cell_names_override is None:
+        cell_props = pd.read_csv(project_folder.inscopix_dir.props_path, header=0, engine='pyarrow')
+        cell_names = cell_props['Name']
+    else:
+        cell_names = cell_names_override
+
+    if cell_contours_override is None:
+        _, cell_contours = DewanJSON.get_outline_coordinates(project_folder.inscopix_dir.contours_path)
+    else:
+        cell_contours = cell_contours_override
+
+    return cell_trace_data, cell_names, cell_contours
+
+
+if __name__ == '__main__':
+    launch_gui()
